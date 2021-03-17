d5093739c07ae27a39f14b6b279a2c939c44022d
sushil bhile <sushilmaxbhile@gmail.com>
2021-03-17 18:48:23 +0530

    check for applicability add in only root master release

--- DDB/applicability.py
+++ DDB/applicability.py
@@ -84,11 +84,12 @@ def update_rootRelease(request):
     return HttpResponse("DONE")
 
 @csrf_exempt
-#def Applicable(request, release):
-def Applicable(request):
+def Applicable(request, Release):
+#def Applicable(request):
     if request.method == "POST":
-        #if release != rootRelease:
-        #    return HttpResponse("YOU CANNOT ADD PLATFORM IN RELEASE OTHER THAN DCX_DMC_MASTER")
+        print(Release)
+        if Release != rootRelease:
+            return HttpResponse("YOU CANNOT ADD PLATFORM IN RELEASE OTHER THAN DCX_DMC_MASTER")
 
         req = json.loads(request.body.decode("utf-8"))
 
