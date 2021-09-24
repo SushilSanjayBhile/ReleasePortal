f184a0934ea967f7aee1aedd323870cdfa63a548
swapnil2 <sshende@diamanti.com>
2021-09-24 11:45:35 +0530

    Sending compressed file to Api in E2E Result Update option.

--- frontend/front-end/src/views/Release/e2eResult/E2EResultUpdate.js
+++ frontend/front-end/src/views/Release/e2eResult/E2EResultUpdate.js
@@ -6,10 +6,10 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { saveUsers } from '../../../actions';
 import { getCurrentRelease } from '../../../reducers/release.reducer';
+import JSZip from 'jszip';
 import Multiselect from 'react-bootstrap-multiselect';
 import { element } from 'prop-types';
 import  CheckBox  from '../../../components/TestCasesAll/CheckBox';
-
 let form = {}
 class UpdateE2EResult extends Component {
     
@@ -28,44 +28,13 @@ class UpdateE2EResult extends Component {
         }
        
     }
-    // handleAllCheckedRelInsert = (event) => {
-    //     let rel = this.state.allReleases1
-    //     rel.forEach(columnName => columnName.isChecked = event.target.checked) 
-    //     this.setState({allReleases1: rel})
-    // }
-   
-    // handleCheckChieldRelInsert = (event) => {
-    //     let rel = this.state.allReleases1
-    //     rel.forEach(columnName => {
-    //         if (columnName.value === event.target.value)
-    //             columnName.isChecked =  event.target.checked
-    //     })
-    //     this.setState({allReleases1: rel})
-    // }
-    // handleAllCheckedRel = (event) => {
-    //     let rele = this.state.rel
-    //     rele.forEach(columnName => columnName.isChecked = event.target.checked) 
-    //     this.setState({rel: rele})
-    // }
-   
-    // handleCheckChieldRel = (event) => {
-    //     let rele = this.state.rel
-    //     rele.forEach(columnName => {
-    //         if (columnName.value === event.target.value)
-    //             columnName.isChecked =  event.target.checked
-    //     })
-    //     this.setState({rel: rele})
-    // }
     componentDidMount() {
-        //this.setState({allReleases1:[]})
         let releaseInfoURL = `/api/release/infoAsc`;
         let rel = []
           axios.get(releaseInfoURL)
             .then(res => {
               res.data.forEach(item => {
                 rel.push(item.ReleaseNumber)
-                //this.state.rel.push({value:item.ReleaseNumber, isChecked:false})
-                // this.props.saveReleaseBasicInfo({ id: item.ReleaseNumber, data: item });
               });
               this.setState({allReleases1:rel})
             }, error => {
@@ -75,13 +44,10 @@ class UpdateE2EResult extends Component {
     getPlatform = (e) => {
         let releaseInfoURL = `/api/releasewiseplatformCli/`+this.state.release;
         let platform = []
-        console.log(releaseInfoURL)
           axios.get(releaseInfoURL)
             .then(res => {
               res.data.forEach(item => {
                 platform.push(item)
-                //this.state.rel.push({value:item.ReleaseNumber, isChecked:false})
-                // this.props.saveReleaseBasicInfo({ id: item.ReleaseNumber, data: item });
               });
               this.setState({platforms:platform,disable:false})
             }, error => {
@@ -89,64 +55,48 @@ class UpdateE2EResult extends Component {
             });
     }
     handleSubmit = (e) => {
-        // e.preventDefault();
-        // e.target.reset();
-        console.log("IN handleSubmit")
         this.setState({
             save : true, disable:true
         })
+        var zip = new JSZip();
         const data = new FormData()
-        data.append('file',this.state.file)
-        console.log("data",data)
+        zip.file(this.state.file.name, this.state.file)
+        //  const data = new FormData()
+        //  data.append('file',this.state.file)
         let req = {
             release:this.state.release,
             platform:this.state.platform
         }
-        axios.post(`/api/execute/${this.state.release}/${this.state.platform}`,data)
-            .then(response=>{
-            //alert(response.data.stdout);
-            console.log(response)
-            //let jsondata = JSON.parse(response.data)
-            //console.log(jsondata)
-            // let str = JSON.stringify(response.data)
-            // str = str.trim()
-            // let da = str.replace(/'/g,'"').replace(/\n/g, '');
-            // console.log(da)
-            // let jsondata = JSON.parse(da)
-            // console.log(jsondata)
-            this.setState({output:response.data})
-            console.log(this.state.output)
-            this.reset();
-        })
-        .catch(err=>{
-            console.log("err",err);
+        zip.generateAsync({type: "blob", compression: "DEFLATE",compressionOptions: {
+           level: 9,
+       }}).then(content => {data.append('file', content);
+            axios.post(`/api/execute/${this.state.release}/${this.state.platform}/${this.state.file.name}`,data)
+                .then(response=>{
+                this.setState({output:response.data})
+                this.reset();
+            })
+            .catch(err=>{
+                console.log("err",err);
+            })
         })
     }
     
     reset = () =>{
-        this.setState({release:'', platform: ''})
+        this.setState({release:'', platform: '',platforms: []})
     }
     handleChange =(e) => {
         const files = e.target.files;
         if (files && files[0]) {
-            this.setState({ file: files[0] });
-            //this.handleFile(files[0]);
+            let t = Math.random().toString().split(".")[1]
+            let temp = new File([files[0]],files[0].name + (new Date().getTime()).toString() + t,{type:files[0].type})
+            this.setState({ file: temp});
         }
       };
 
     render() {
-        // this.state.allReleases1.sort()
-        //this.state.rel.sort()
-        //let rel = this.state.allReleases1 ? this.state.allReleases1.map(item => ({ value: item, selected: this.state.selectedReleases && this.state.selectedReleases.includes(item) })) : [];
-        //let relEdit = this.state.allReleases1 ? this.state.allReleases1.map(item => ({ value: item, selected: this.state.selectedReleasesEdit && this.state.selectedReleasesEdit.includes(item) })) : [];
-        // let relRemove = this.state.assignedR ? this.state.assignedR.map(item => ({ value: item, selected: this.state.selectedReleasesRemove && this.state.selectedReleasesRemove.includes(item) })) : [];
-        // let multiselect = {/*'Releases': rel, 'SelectedReleases': relEdit,*/ "RemoveReleases": relRemove};
-        //let platforms = this.props.selectedRelease && this.props.selectedRelease.PlatformsCli ? this.props.selectedRelease.PlatformsCli : []
         if(this.state.platforms){
             this.state.platforms.sort()
         }
-        
-        console.log(this.state.disable)
         return(
             <div>
             {
@@ -155,7 +105,7 @@ class UpdateE2EResult extends Component {
                     <Col xs="11" sm="11" md="11" lg="11" className="rp-summary-tables" style={{ 'margin-left': '1.5rem' }}>
                         <div className='rp-app-table-header' style={{ cursor: 'pointer' }} onClick={() => this.setState({ Open: !this.state.Open })}>
                             <div class="row">
-                                <div class='col-lg-12'>
+                                {/* <div class='col-lg-12'> */}
                                     <div style={{ display: 'flex' }}>
                                         <div style={{ display: 'inlineBlock' }}>
                                             {
@@ -170,7 +120,7 @@ class UpdateE2EResult extends Component {
                                             <span className='rp-app-table-title'>E2E Result Updation</span>
                                         </div>
                                     </div>
-                                </div>
+                                {/* </div> */}
                             </div>
                         </div>
                         <Collapse isOpen={true}>
@@ -200,7 +150,7 @@ class UpdateE2EResult extends Component {
                                                 <Row xs="6" md="3" lg="3">
                                                     {
                                                             <Input style={{ borderColor: this.state.errors['release'] ? 'red' : '' }} className='rp-app-table-value' type="select" id="release" name="release" value={this.state.release}
-                                                                onChange={(e) => this.setState({ release: e.target.value , errors: { ...this.state.errors, release: null }},() => this.getPlatform())} >
+                                                                onChange={(e) => this.setState({ release: e.target.value , errors: { ...this.state.errors, release: null }},() => {this.getPlatform();this.setState({output:''})})} >
                                                                 <option value=''>Select Release</option>
                                                                 {
                                                                     this.state.allReleases1.map(item => <option value={item}>{item}</option>)
