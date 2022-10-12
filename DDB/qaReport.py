9e56fe90adffdc677441e78a348b69ee033a56cd
sushil bhile <sushilmaxbhile@gmail.com>
2022-10-12 03:40:20 -0700

    Added backend changes for sdet release report

--- DDB/qaReport.py
+++ DDB/qaReport.py
@@ -6,7 +6,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from .views import RELEASEINFO
 from .serializers import RELEASE_SERIALIZER, LOG_SERIALIZER, GUI_LOGS_SERIALIZER
-from .models import RELEASES, LOGS, LOGSGUI
+from .models import RELEASES, LOGS, LOGSGUI, TC_INFO, TC_INFO_GUI
+from django.db.models import Count
+from django.db.models import Q
 @csrf_exempt
 def getQAReport(request):
     if request.method == "GET":
@@ -104,4 +106,99 @@ def RESULT_LOGS_GUI(Release, sdate, edate):
                             pass
             except:
                 pass
+    return user
+
+@csrf_exempt
+def getSDETReleaseReport(request):
+    output = {}
+    outputGui = {}
+    automated = {}
+    if request.method == "GET":
+        releases = request.GET.getlist('releases[]')
+        output = getCliExecuted(releases)
+        outputGui = getGuiExecuted(releases)
+        automated = getAutomatedCount()
+        assignedCli = getCliAssigned(releases)
+        assignedGui = getGuiAssigned(releases)
+    return JsonResponse({"SDETRelReport": {"CLIAssigned": assignedCli, "GuiAssigned": assignedGui, "CLIExec": output, "GuiExec": outputGui, "Automated": automated}}, status = 200)
+
+def getCliExecuted(Release):
+    data = {}
+    for relNum in Release:
+        if relNum != "TestDatabase":
+            result = LOGS.objects.values('TcID', 'CardType', 'UserName', 'LogData').filter(LogData__contains = 'Status Added').annotate(Count('logNo')).using(relNum)
+            data[relNum] = {}
+            data = iterResult(result, data, relNum)
+    return data
+
+def getGuiExecuted(Release):
+    data = {}
+    for relNum in Release:
+        if relNum != "TestDatabase":
+            result = LOGSGUI.objects.values('tcInfoNum', 'UserName', 'LogData').filter(LogData__contains = 'Status Added').annotate(Count('logNo')).using(relNum)
+            data[relNum] = {}
+            data = iterResult(result, data, relNum)
+    return data
+
+def getCliAssigned(Release):
+    data = {}
+    for relNum in Release:
+        if relNum != "TestDatabase":
+            result = TC_INFO.objects.values().filter(Q(applicable__contains = "Applicable") & ~Q(Assignee__contains = "UNKNOWN")).using(relNum)
+            data[relNum] = {}
+            data = iterForAssignee(result, data, relNum)
+    return data
+
+def getGuiAssigned(Release):
+    data = {}
+    for relNum in Release:
+        if relNum != "TestDatabase":
+            result = TC_INFO_GUI.objects.values().filter(Q(applicable__contains = "Applicable") & ~Q(Assignee__contains = "UNKNOWN")).using(relNum)
+            data[relNum] = {}
+            data = iterForAssignee(result, data, relNum)
+    return data
+
+def iterForAssignee(result, data, relNum):
+    for log in result:
+        user = log["Assignee"]
+        if user not in data[relNum]:
+            data[relNum][user] = {}
+            data[relNum][user]["assigned"] = 1
+            data[relNum][user]["time"] = log["Time"]
+        else:
+            data[relNum][user]["assigned"] = data[relNum][user]["assigned"] + 1
+            data[relNum][user]["time"] = data[relNum][user]["time"] + log["Time"]
+    return data
+
+def iterResult(result, data, relNum):
+    for log in result:
+        user = log["UserName"]
+        if user not in data[relNum]:
+            data[relNum][user] = 1
+        else:
+            data[relNum][user] = data[relNum][user] + 1
+    return data
+
+def getAutomatedCount():
+    user = {}
+    data = LOGS.objects.using('DCX-DMC-Master').filter(LogData__contains = '"old":"TC NOT AUTOMATED"').all()
+    serializer = LOG_SERIALIZER(data, many = True)
+    user = iterSerializerForAuto(serializer, user)
+    data = LOGSGUI.objects.using('DCX-DMC-Master').filter(LogData__contains = '"old":"TC NOT AUTOMATED"').all()
+    serializer = GUI_LOGS_SERIALIZER(data, many = True)
+    user = iterSerializerForAuto(serializer, user)
+    return user
+
+def iterSerializerForAuto(serializer, user):
+    for log in serializer.data:
+        try:
+            logdata = json.loads(log['LogData'])
+            if logdata["TcName"]["old"] == "TC NOT AUTOMATED" and logdata["TcName"]["new"] != "TC NOT AUTOMATED" :
+                if log["UserName"] not in user:
+                    user[log["UserName"]] = {}
+                    user[log["UserName"]]["auto"] = 1
+                else:
+                    user[log["UserName"]]["auto"] = user[log["UserName"]]["auto"] + 1
+        except:
+            pass
     return user
